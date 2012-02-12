ed8dfe9637435d07f6c12c7c2615a0b68cbd0e3a
enpysoft <enpysoft@gmail.com>
2012-02-12 09:09:27 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '8243f3f70231973d0430fc0be60f8b441e2eb6c0cc56054adbe7210865d3ab35b1ffff24ba3635b94916eb9303833f85960dffe158ef245da5e04414fbb54acf'
